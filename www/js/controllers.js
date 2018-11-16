7e4d62bff57d790adaa9fe0f7c51a00efeb11031
none Frontier <liuyiquancov5@gmail.com>
2018-11-16 11:18:33 +0800

    add google map

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -21,15 +21,38 @@ function ($scope, $stateParams) {
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
 
-
 }])
    
 .controller('mapCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
+	var LatLng = new google.maps.LatLng(22.341072, 114.179645);
 
+	var mapOptions = {
+		center:LatLng,
+		zoom:17,
+		mapTypeId:google.maps.MapTypeId.ROADMAP
+	};
+	$scope.map = new google.maps.Map(document.getElementById('map'),mapOptions);
 
+	//annotation (marker)
+	google.maps.event.addListenerOnce($scope.map,'idle',function() {
+		latLng = new google.maps.LatLng(22.341072, 114.179645);
+		var marker = new google.maps.Marker({
+			map:$scope.map,
+			animation:google.maps.Animation.DROP,
+			position:latLng
+		});
+		//infowindow
+		var infoWindow = new google.maps.InfoWindow({
+			content:'AC Hall'
+		});
+
+		google.maps.event.addListener(marker,'click',function() {
+			infoWindow.open($scope.map,marker);
+		});
+	});
 }])
       
 .controller('aboutUsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
